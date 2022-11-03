aa352f92a7200e7da92de5498cd605bbf399edbd
cassioestevao <cassioestevao@users.noreply.github.com>
2022-11-03 12:27:41 -0300

    Apresentação

---
+++ Hellow World.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	printf("Ola me chamo Cassio e sou estudante de ADS \n");
+	printf("Prentendo ser um programar revolucionario!!! \n");
+	
+	system("pause");
+}
