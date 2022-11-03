da8b5a2c0b44a8fe1cbdb0cacd3edbf3d3f939a6
Cássio Estevão <116197477+cassioestevao@users.noreply.github.com>
2022-11-03 12:31:28 -0300

    Delete Hellow World.cpp

--- Hellow World.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main (){
-	printf("Ola me chamo Cassio e sou estudante de ADS \n");
-	printf("Prentendo ser um programar revolucionario!!! \n");
-	
-	system("pause");
-}
